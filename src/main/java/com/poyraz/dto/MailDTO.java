74f08f3a477fbf07c5dbedbc7fad8d989b16fee7
poyrazemun <ofatih-poyraz@hotmail.com>
2025-09-07 07:24:29 +0200

    MailDTO record is edited and firstName and templateName parameters are added.

--- src/main/java/com/poyraz/dto/MailDTO.java
+++ src/main/java/com/poyraz/dto/MailDTO.java
@@ -1,5 +1,5 @@
 package com.poyraz.dto;
 
-public record MailDTO(String to, String subject, String body) {
+public record MailDTO(String to, String subject, String body, String templateName, String firstName) {
 
 }
