32fee3992d471863b979c1f0b171907ac0f85c6e
poyrazemun <ofatih-poyraz@hotmail.com>
2025-09-08 07:45:50 +0200

    Added @EnableAsync annotation to enable asynchronous method execution capability.

--- src/main/java/com/poyraz/ZmailSpringBootApplication.java
+++ src/main/java/com/poyraz/ZmailSpringBootApplication.java
@@ -3,7 +3,9 @@ package com.poyraz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @PropertySource("classpath:messages.properties")
 @SpringBootApplication
 public class ZmailSpringBootApplication {
